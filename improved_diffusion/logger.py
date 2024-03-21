8d289eccf38e21d19d1456dd7e3ea3617ea46354
Hai La <latruonghai2507@icloud.com>
2024-03-21 12:42:31 +0700

    log output format

--- improved_diffusion/logger.py
+++ improved_diffusion/logger.py
@@ -466,6 +466,7 @@ def configure(dir=None, format_strs=None, comm=None, log_suffix=""):
             format_strs = os.getenv("OPENAI_LOG_FORMAT_MPI", "log").split(",")
     format_strs = filter(None, format_strs)
     output_formats = [make_output_format(f, dir, log_suffix) for f in format_strs]
+    print(f"==>> output_formats: {output_formats}")
 
     Logger.CURRENT = Logger(dir=dir, output_formats=output_formats, comm=comm)
     if output_formats:
